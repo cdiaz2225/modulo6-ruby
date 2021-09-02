5b03da2aa260ddbcb1cec433f803b15634875252
cdiaz <cdiaz2225@gmail.com>
2021-09-01 23:05:17 -0400

    ejercicios for

--- ejercicio1/ejercicios_while.rb
+++ ejercicio1/ejercicios_while.rb
@@ -8,20 +8,53 @@
 #     puts "dijiste #{texto}"
 
 # end
-suma=0
-dato=0
-puts "Ingresa monto"
-while dato.to_i !=-1
+# suma=0
+# dato=0
+# puts "Ingresa monto"
+# while dato.to_i !=-1
     
-    dato=gets.chomp
-    if dato.to_i != -1
-    suma += dato.to_i
-    end
-end
-puts suma
-texto = gets.chomp
-letras = *('a'..'z')
-foreach letra in letras
-if texto.include? letra
-puts letras
-end
+#     dato=gets.chomp
+#     if dato.to_i != -1
+#     suma += dato.to_i
+#     end
+# end
+# puts suma
+# texto = gets.chomp
+# letras = *('a'..'z')
+# foreach letra in letras
+# if texto.include? letra
+# puts letras
+# end
+
+# # Usuario Versus CPU
+# pares=0
+# impares=0
+# puts "Ingresa Nombre de Monstruo"
+# nombremostruo=gets
+# vidademonstruo=rand(100..500)
+# puts "Vida del monstruo es", vidademonstruo
+# while (vidademonstruo!=0)
+#     if (vidademonstruo  % 2 ==0)
+#         vidademonstruo+= -1
+#         pares+=1
+#     else
+#         vidademonstruo=(vidademonstruo/2)
+#         impares+=1
+#     end
+
+# end
+# puts "Intentos Pares ",pares
+# puts "intentos Impares",impares
+
+# numero = rand(1..20)
+# puts "Adivina cual es el número que estoy pensando"
+# adivinar=gets.chomp.to_i
+
+# if numero>adivinar
+#     puts"muy abajo"
+# if numero=adivinar
+#     puts "felicitaciones"
+# else
+#     puts"muy arriba
+# end
+
