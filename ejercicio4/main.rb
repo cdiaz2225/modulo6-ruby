f8e4474e5f8773c587ccdf15027a8a4620ce10c0
cdiaz <cdiaz2225@gmail.com>
2021-09-10 20:17:52 -0300

    Menu

--- ejercicio4/main.rb
+++ ejercicio4/main.rb
@@ -3,9 +3,8 @@ require_relative 'modulo_impresiones'
 
 include ModuloUsuarios
 include ModuloImpresiones
+include Extras
 
-arreglo_usuarios_correo_faker = generar_usuarios_con_correo(5)   # módulo usuarios
-imprimir_usuarios_con_correos(arreglo_usuarios_correo_faker)    # módulo impresiones
+menu_usuario
 
-arreglo_usuarios_con_gustos = generar_usuarios_con_gustos(5)
-imprimir_usuarios_con_gustos(arreglo_usuarios_con_gustos)
+
--- ejercicio4/modulo_usuarios.rb
+++ ejercicio4/modulo_usuarios.rb
@@ -18,3 +18,32 @@ module ModuloUsuarios
         return usuarios
     end
 end
+
+module Extras
+    
+def menu_usuario()
+    opcion = 0
+    while opcion != 3 
+    puts "1. Datos 1"
+    puts "2. Datos 2"
+    puts "3. Salir"
+    opcion=gets.to_i
+
+    if opcion == 1
+        puts "Ingrese la Cantidad de usuarios a desplegar"
+        n = gets.to_i
+        arreglo_usuarios_correo_faker = generar_usuarios_con_correo(n)
+        imprimir_usuarios_con_correos(arreglo_usuarios_correo_faker)
+    elsif 
+    opcion ==2
+        puts "Ingrese la Cantidad de usuarios a desplegar"
+        n = gets.to_i
+        arreglo_usuarios_con_gustos = generar_usuarios_con_gustos(5)
+        imprimir_usuarios_con_gustos(arreglo_usuarios_con_gustos)
+    elsif opcion == 3
+        puts "Gracias por Preferirnos"
+    end
+    end
+end
+end
+
